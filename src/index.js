4e478f9ade7a91d21ccaa1a0ee9339258ea18700
gregxsunday <gregxsunday@gmail.com>
2022-06-24 15:47:50 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import MetaMaskOnboarding from '@metamask/onboarding'
+
+const currentUrl = new URL(window.location.href)
+const forwarderOrigin = currentUrl.hostname === 'localhost'
+  ? 'http://localhost:8889'
+  : undefined
+
+var new_window;
+
+function openWindowConnect(){
+  new_window = window.open('/connect-account.html', '_blank', 'location=yes,height=1080,width=1920,scrollbars=yes,status=yes')
+}
+
+function openWindowTransact(){
+  new_window = window.open('/transact.html', '_blank', 'location=yes,height=1080,width=1920,scrollbars=yes,status=yes')
+}
+
+const initialize = async () => {
+  const { ethereum } = window
+
+  const transactionParameters = {
+    nonce: '0x00', // ignored by MetaMask
+    gasPrice: '0x09184e72a000', // customizable by user during MetaMask confirmation.
+    gas: '0x2710', // customizable by user during MetaMask confirmation.
+    to: '0x0000000000000000000000000000000000000000', // Required except during contract publications.
+    from: '0x89B8b196D1a9abdb0FE8CDB4b57eE13b0C6755d3', // must match user's active address.
+    value: '0x00', // Only required to send ether to the recipient from the initiating external account.
+    data:
+      '0x7f7465737432000000000000000000000000000000000000000000000000000000600057', // Optional, but used for defining smart contract creation and interaction.
+    chainId: '0x3', // Used to prevent transaction reuse across blockchains. Auto-filled by MetaMask.
+  };
+
+  ethereum.request({method: 'eth_requestAccounts'}).then(() => {
+    new_window.close();
+    ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [transactionParameters],
+    });
+    setTimeout(openWindowTransact, 500)
+  })
+  setTimeout(openWindowConnect, 500)
+}
+
+window.addEventListener('DOMContentLoaded', initialize)
